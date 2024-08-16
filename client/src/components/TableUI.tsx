3e5f0b88249070b0b1db16a8ebdf9855d53478f4
EfosaE <osamwonyiefosa02@gmail.com>
2024-08-16 21:54:26 +0100

    changed the show entires location

--- client/src/components/TableUI.tsx
+++ client/src/components/TableUI.tsx
@@ -7,12 +7,13 @@ import { Contact } from '../utils/columnsDefs';
 interface HeaderProps {
   globalFilter: string;
   setGlobalFilter: (filterValue: string) => void;
+  table: Table<Contact>;
 }
 
 type TableFooterProps = {
   table: Table<Contact>;
 };
-export const TableHeader = ({ globalFilter, setGlobalFilter }: HeaderProps) => {
+export const TableHeader = ({ globalFilter, setGlobalFilter, table  }: HeaderProps) => {
   const modalRef = useRef<HTMLDialogElement>(null);
 
   const openModal = () => {
@@ -27,10 +28,16 @@ export const TableHeader = ({ globalFilter, setGlobalFilter }: HeaderProps) => {
     <div className='flex md:flex-row md:justify-between p-4 flex-col gap-2'>
       <div className='flex items-center gap-2'>
         <p>show</p>
-        <select className='select w-fit'>
-          <option>10</option>
-          <option>20</option>
-          <option>30</option>
+        <select
+          value={table.getState().pagination.pageSize}
+          onChange={(e) => {
+            table.setPageSize(Number(e.target.value));
+          }}>
+          {[10, 20, 30, 40, 50].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              {pageSize}
+            </option>
+          ))}
         </select>
         <p>entries</p>
       </div>
@@ -59,19 +66,6 @@ export const TableFooter = ({ table }: TableFooterProps) => {
 
   return (
     <div className='flex items-center justify-between py-1'>
-      <label htmlFor=''>
-        <select
-          value={table.getState().pagination.pageSize}
-          onChange={(e) => {
-            table.setPageSize(Number(e.target.value));
-          }}>
-          {[10, 20, 30, 40, 50].map((pageSize) => (
-            <option key={pageSize} value={pageSize}>
-              {pageSize}
-            </option>
-          ))}
-        </select>
-      </label>
       <div className='join mx-auto'>
         {Array.from({ length: pageCount }, (_, index) => (
           <button
